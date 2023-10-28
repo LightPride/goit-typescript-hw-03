b78e7bf9d2313106d32c3be894108946676182d8
LightPride <vlad.grinevich1@gmail.com>
2023-10-28 18:24:40 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  abstract openDoor(key: Key): void;
+  comeIn(person: Person) {
+    if (this.door) {
+      const tenants: Person[] = [];
+      tenants.push(person);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door === true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +49,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
